460b2fc036f617b83f96d3525c64a3a8e1d25a9b
vaishu542 <159023413+vaishu542@users.noreply.github.com>
2024-02-28 18:15:30 +0530

    Update Question1.java

--- src/Question1.java
+++ src/Question1.java
@@ -1,12 +1,11 @@
-// Superclass
+
 class Shape {
-    // Method to calculate the area (to be overridden by subclasses)
     public double calculateArea() {
         return 0.0;
     }
 }
 
-// Subclass Circle
+
 class Circle extends Shape{
     private double radius;
 
@@ -15,25 +14,21 @@ class Circle extends Shape{
         this.radius = radius;
     }
 
-    // Override calculateArea() method for circles
     @Override
     public double calculateArea() {
         return Math.PI * radius * radius;
     }
 }
 
-// Subclass Rectangle
 class Rectangle extends Shape {
     private double length;
     private double width;
 
-    // Constructor
     public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
-    // Override calculateArea() method for rectangles
     @Override
     public double calculateArea() {
         return length * width;
@@ -47,8 +42,7 @@ public class Question1 {
         shapes[0] = new Circle(5.0);
         shapes[1] = new Rectangle(4.0, 6.0);
         shapes[2] = new Circle(3.0);
-
-        // Iterate through the array and print the area of each shape
+        
         for (Shape shape : shapes) {
             System.out.println("Area: " + shape.calculateArea());
         }
